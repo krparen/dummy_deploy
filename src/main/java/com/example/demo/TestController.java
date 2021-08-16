3731d825df818196f96ce385148a968140ac052e
krparen <ksacck@gmail.com>
2021-08-17 00:08:04 +0700

    hello i'm first commit

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping
+    public String testEndpoint() {
+        return "VERY OLD Hello from test service!";
+    }
+}
